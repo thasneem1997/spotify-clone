6075b1c35dd001037ddc5db63f77961bee922962
fathimaththasneem <fathimaththasnee@gmail.com>
2024-06-09 19:55:16 +0530

    modified

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,28 +1,22 @@
-import React from 'react'
-import { Avatar } from '@material-ui/core';
-import { useDataLayerValue } from './DataLayer'
+import React from "react";
+import { Avatar } from "@material-ui/core";
+import { useDataLayerValue } from "./DataLayer";
 import { Search } from "@material-ui/icons";
 
 function Header() {
-  const [{user}, dispatch] = useDataLayerValue(); 
+  const [{ user }, dispatch] = useDataLayerValue();
   return (
-   
-
-    <div className='header'>
-        <div className='header__left'>
-            <Search/>
-            <input type="text" placeholder='Search for an artist, album or song' />
-
-        </div>
-        <div className='header__right'>
-            <Avatar src={user?.images[0]?.url}
-            alt={'user?.display_name'}/>  
-            <h4>{user?.display_name}</h4>
-                  </div>
-
-      
+    <div className="header">
+      <div className="header__left">
+        <Search />
+        <input type="text" placeholder="Search for an artist, album or song" />
+      </div>
+      <div className="header__right">
+        <Avatar src={user?.images[0]?.url} alt={"user?.display_name"} />
+        <h4>{user?.display_name}</h4>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
